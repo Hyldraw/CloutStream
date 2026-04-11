be9b9e41af30adffa0385226affcd9c122eca8fe
Hyldraw <112813424+Hyldraw@users.noreply.github.com>
2026-04-11 20:52:48 +0100

    Update formatTitle.js

--- formatTitle.js
+++ formatTitle.js
@@ -2,7 +2,7 @@
 function formatTitle({ name, service, flags }) {
     const line1 = `🌊 ${name}`;
     const line2 = `📦 ${service}`;
-    const line3 = `🌎 ${flags.map(f => "\ " + f).join(" ")}`;
+    const line3 = `🌎 ${flags.map(f => " " + f).join("•")}`;
     return `${line1}\n${line2}\n${line3}`;
 }
 
