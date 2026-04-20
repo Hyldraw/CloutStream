b354977a82ac6edba99add8ff86e966471c55dbf
Hyldraw <112813424+Hyldraw@users.noreply.github.com>
2026-04-20 09:28:25 +0100

    Atualizar o CMMovies.js

--- CMMovies.js
+++ CMMovies.js
@@ -2,6 +2,19 @@
 const formatTitle = require("./CMformatTitle");
 
 const movies = {
+// Jovem Obsessão (1994)
+
+"tt0110770": [
+  {
+    name: "🌊 CloutStream 480p",
+    title: formatTitle({
+      name: "Jovem Obsessão (1994)",
+      service: "Terabox",
+      flags: ["Português"],
+      by: "From: Rodriguez061199"
+    }),
+    url: "https://cyler3.vrovegeta.workers.dev/?url=CxwRFR5JWl8TFgNLFw0XBA8cDV4FEQRKDBgACwwDHF8XCRUXBkcWER8WFB0NDxNaFgNYUV5KTUhVUkBcV1xSUUsAHREWBB0BXltcUFlDQUdUUkVRRRwcFQhOOEMxWSskNjwqOllLRVYCCBBYWlhTVl5BTEFcV0JQUFpSQx4aEh5ZUkUGVVkAAQxHRhJUVEdSBQxTVl9CFkhcWExXUApXVVUVEEZRB0cBAU4RDAAWBgQFDARYUl9SU1tERUhWWVIPEDwKDggdSFhNQUlbQzNLS0MyBwIFGFxTV0E4SwASBVhMSElbSyUEEQVdBxEKBRsIS0FPVFsPRVlKFRs2FxoMCwpbREZNSFoPDAELTUpUXF4QDiEVEw0XJgwAEFhNRxEWD1VUQwQABRwFGBEXXllDAAUDBk1VRxcJCg0LERkKBRVZUVIEExg6DAlOR0VUVEZdRR8AB1BCUxMMABoLBgRYARgRGgg#tgWebAppData=query_id%3DAAH1ovEpAwAAAPWi8SmvSwAj%26user%3D%257B%2522id%2522%253A7146152693%252C%2522first_name%2522%253A%2522David%2522%252C%2522last_name%2522%253A%2522%2522%252C%2522username%2522%253A%2522Davidzrrs%2522%252C%2522language_code%2522%253A%2522pt-pt%2522%252C%2522allows_write_to_pm%2522%253Atrue%252C%2522photo_url%2522%253A%2522https%253A%255C%252F%255C%252Ft.me%255C%252Fi%255C%252Fuserpic%255C%252F320%255C%252F78FHIIhuuU2G0FrKEDhwzzDrCBrYoFtCCa5Etj5z28q92gk_2UkbpRI4RwCTP63r.svg%2522%257D%26auth_date%3D1776670827%26signature%3DspDIQUex_QQ9jcgIB6aU36LmBYXYBExKRFk5bBHclirJ5sjM4bJpEX7kr19Lt6l8g3qGretbdzY7eXV8GVeNDw%26hash%3Df4d06f086a50e60fb12fa0287eadef677a8b5fba77fccfebe458118c5f33ade3&tgWebAppVersion=9.5&tgWebAppPlatform=web&tgWebAppThemeParams=%7B%22bg_color%22%3A%22%23ffffff%22%2C%22button_color%22%3A%22%233390ec%22%2C%22button_text_color%22%3A%22%23ffffff%22%2C%22hint_color%22%3A%22%23707579%22%2C%22link_color%22%3A%22%2300488f%22%2C%22secondary_bg_color%22%3A%22%23f4f4f5%22%2C%22text_color%22%3A%22%23000000%22%2C%22header_bg_color%22%3A%22%23ffffff%22%2C%22accent_text_color%22%3A%22%233390ec%22%2C%22section_bg_color%22%3A%22%23ffffff%22%2C%22section_header_text_color%22%3A%22%233390ec%22%2C%22subtitle_text_color%22%3A%22%23707579%22%2C%22destructive_text_color%22%3A%22%23df3f40%22%7D"
+],
 
 };
 
