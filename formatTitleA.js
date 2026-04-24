335167f9fe7091ff4e9b36dee1660a48906dc452
Hyldraw <112813424+Hyldraw@users.noreply.github.com>
2026-04-24 18:21:55 +0100

    Add formatTitle function to format title details

---
+++ formatTitleA.js
@@ -0,0 +1,10 @@
+// formatTitleA.js
+function formatTitle({ name, ep, service, flags }) {
+    const line1 = `🍙 ${name}`;
+    const line2 = `🪪 ${ep}`;
+    const line3 = `📦 ${service}`;
+    const line4 = `🌎 ${flags.map(f => " " + f).join(" •")}`;
+    return `${line1}\n${line2}\n${line3}\n${line4}`;
+}
+
+module.exports = formatTitle;
